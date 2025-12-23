666b9cd3beecfe9bfb2d58f839228390433261b6
junye6666666 <2726257575@qq.com>
2025-12-23 08:43:30 +0800

    no message

--- src/api/ship.js
+++ src/api/ship.js
@@ -1,21 +1,22 @@
 import request from '@/utils/request.js'
 
 // 1. 获取船舶列表 (带搜索条件)
+// ✅ 修复点：后端 Controller 用的是 /ships (复数)，这里必须对应！
 export const shipListService = (params) => {
-    return request.get('/ship', { params })
+    return request.get('/ships', { params })
 }
 
 // 2. 新增船舶
 export const shipAddService = (shipData) => {
-    return request.post('/ship', shipData)
+    return request.post('/ships', shipData)
 }
 
 // 3. 修改船舶
 export const shipUpdateService = (shipData) => {
-    return request.put('/ship', shipData)
+    return request.put('/ships', shipData)
 }
 
 // 4. 删除船舶
 export const shipDeleteService = (id) => {
-    return request.delete('/ship', { params: { id } })
+    return request.delete('/ships', { params: { id } })
 }
